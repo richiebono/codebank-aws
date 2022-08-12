be71bbace27c998a2b3b5ba0d01a56b63682ac2b
Richard Bono <Richard.Bono.CTR@aiworldwide.com>
2022-08-12 12:56:05 -0300

    first commit

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,7 @@
+prefix = "bono"
+cluster_name = "codebank-cluster"
+retention_days = 30
+desired_size = 2
+max_size = 4
+min_size = 2
+vpc_cidr_block = "10.0.0.0/16"
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {}
+variable "vpc_cidr_block" {}
+variable "cluster_name" {}
+variable retention_days {}
+variable "desired_size" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
