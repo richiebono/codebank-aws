2d64286b696574f955612576a3fec71b79e1325a
richiebono <richiebono@gmail.com>
2022-08-22 19:19:13 +0200

    Create cluster with officia documentation.

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "${VAR.AWS_CLUSTER_NAME}-eks-${random_string.suffix.result}"
+  cluster_name = "${var.AWS_CLUSTER_NAME}-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
